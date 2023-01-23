8c0d22e9db9519fb4f601ad2b64d43075f0b5ae8
RomainC75 <rom.chenard@gmail.com>
2023-01-23 23:36:24 +0100

    section 8.1

---
+++ section8/server.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use('/products',require('./routes/products.route'))
+app.use('/cart',require('./routes/cart.route'))
+
+app.use(shopRoutes);
+
+
+app.use(errorController.get404);
+
+app.listen(3000);
